18850d50c2d64af6b039b453668a42d7943645db
iashil <asyl68372@gmail.com>
2026-09-12 02:40:20 +0300

    Fix import path for Toaster component

--- client/src/components/ui/App.tsx
+++ client/src/components/ui/App.tsx
@@ -1,4 +1,4 @@
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "./components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Admin from "@/pages/Admin";
 import NotFound from "@/pages/NotFound";
