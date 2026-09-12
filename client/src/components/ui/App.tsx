f9a1b7d1c2a8efc7cb70337fac46290f75861bce
iashil <asyl68372@gmail.com>
2026-09-12 03:26:22 +0300

    Fix import path for Admin component

--- client/src/components/ui/App.tsx
+++ client/src/components/ui/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
-import Admin from "@/pages/Admin";
+import Admin from "./pages/Admin";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
